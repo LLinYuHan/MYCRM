0e9862458095caaf302e0073af8e003c0ef51bc0
yuhan <50071797+LINYUHAN1288@users.noreply.github.com>
2020-01-04 00:44:51 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=crm',
     'username' => 'root',
-    'password' => '847849726lyh',
+    'password' => '',
     'charset' => 'utf8',
     'tablePrefix' => 'crm_',
     // Schema cache options (for production environment)
